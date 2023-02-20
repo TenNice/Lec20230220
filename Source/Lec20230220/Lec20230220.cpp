7e2a91c16e41d1785a705bc93160c2dd7b48eeb0
kosihoon <kosihoon1@gmail.com>
2023-02-20 09:26:02 +0900

    first

---
+++ Source/Lec20230220/Lec20230220.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lec20230220.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lec20230220, "Lec20230220" );
